f9425ba42448ea3e56967ae47af06bb27626f41d
Luckywolf82 <trygvetrygg@hotmail.com>
2026-03-15 14:47:32 +0100

    Add STARTER_KIT_VERSION for GovernanceHub

---
+++ starter-kit/src/components/governance/STARTER_KIT_VERSION.jsx
@@ -0,0 +1,26 @@
+/**
+ * STARTER KIT VERSION
+ *
+ * Used by GovernanceHub to detect governance version
+ * installed in a repository.
+ */
+
+export const STARTER_KIT_VERSION = {
+  version: "1.0.0",
+  name: "GovernanceHub Starter Kit",
+  releaseDate: "2026-03-15",
+
+  description:
+    "Initial governance bootstrap package for AI-governed repositories.",
+
+  components: [
+    "AI_STATE",
+    "AI_PROJECT_INSTRUCTIONS",
+    "LockedFiles",
+    "NextSafeStep",
+    "PhaseExecutionLog",
+    "AUDIT_INDEX",
+    "AUDIT_SYSTEM_GUIDE",
+    "PROJECT_REGISTRY"
+  ]
+};
